73ee0908c886d6f21f8e770e6d9512b020cd3c80
TautvydasVck <tautvydas.vck@protonmail.com>
2023-03-13 09:11:03 +0200

    base upload

--- event-report-test.py
+++ event-report-test.py
@@ -10,16 +10,24 @@ def Main():
     name = input()
     pswd = getpass()
     jsonFile = GetConfigFile()
-    ReadEvents(jsonFile)
+    #ReadEvents(jsonFile)
+    with open(jsonFile) as f:
+        data = json.load(f)
     # print("value: {}".format(jsonFile))
-
+    
+def LoginToDevice(data):
+    ip = data["info"]["ip-address"]
 
 def GetConfigFile():
     return askopenfilename()
     # return print("File is in: {}".format(fileLocation))
 
-def ReadEvents(file):
-    data = json.dumps(file)
+def CheckForMobile(ip, data):    
+    #device's info
+    model=data["info"]["product"]    
+
+#def ReadEvents(file):
+#    data = json.dumps(file)
     
 
 Main()
