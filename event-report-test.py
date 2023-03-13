08a8975ae1a69f8e568d1c25fe4ed503fc7996b8
TautvydasVck <tautvydas.vck@protonmail.com>
2023-03-13 10:49:24 +0200

    update

--- event-report-test.py
+++ event-report-test.py
@@ -1,33 +1,51 @@
 # imports
 import json
+import requests
 from getpass import getpass
 from tkinter.filedialog import askopenfilename
 
 # main function with user login
+
+
 def Main():
-    print("Enter router's admin page credentials")
-    print("Username: ", end="")
-    name = input()
-    pswd = getpass()
-    jsonFile = GetConfigFile()
-    #ReadEvents(jsonFile)
-    with open(jsonFile) as f:
-        data = json.load(f)
+    print(end="-----------\n")
+    token = LoginToDevice()
+    data = GetConfigData()
     # print("value: {}".format(jsonFile))
-    
-def LoginToDevice(data):
-    ip = data["info"]["ip-address"]
 
-def GetConfigFile():
-    return askopenfilename()
-    # return print("File is in: {}".format(fileLocation))
 
-def CheckForMobile(ip, data):    
-    #device's info
-    model=data["info"]["product"]    
+def GetConfigData():
+    # jsonFile = askopenfilename()
+    # with open(jsonFile) as f:
+    with open("/home/studentas/Documents/Python/Automated_tests/2-nd task/Teltonika_event-reporting-testing/event-config.json") as f:
+        data = json.load(f)
+    return data
 
-#def ReadEvents(file):
+
+def LoginToDevice():
+    # print("Enter router's admin page credentials")
+    # print("Username: ", end="")
+    # name = input()
+    # pswd = getpass()
+    name = "admin"
+    pswd = "Admin123"
+    # print("Enter router's IP address")
+    # print("IP: ", end="")
+    # ipAddr = input()
+    ipAddr = "192.168.1.1"
+
+    url = "http://"+ipAddr+"/api/login"
+    creds = {"username": name, "password": pswd}
+    response = requests.post(url, json=creds).json()
+    return response["jwtToken"]
+
+
+def CheckForMobile(ip, data):
+    # device's info
+    model = data["info"]["product"]
+
+# def ReadEvents(file):
 #    data = json.dumps(file)
-    
+
 
 Main()
