0a27fe4721f56fee8ff997bacd02c8c747fd8530
TautvydasVck <tautvydas.vck@protonmail.com>
2023-03-13 13:33:23 +0200

    update

--- event-report-test.py
+++ event-report-test.py
@@ -1,17 +1,50 @@
 # imports
 import json
+import sys
 import requests
 from getpass import getpass
 from tkinter.filedialog import askopenfilename
 
-# main function with user login
-
 
 def Main():
+    reqsData = RequestData()
     print(end="-----------\n")
-    token = LoginToDevice()
-    data = GetConfigData()
-    # print("value: {}".format(jsonFile))
+    # print("Enter router's admin page credentials")
+    # print("Username: ", end="")
+    # name = input()
+    # pswd = getpass()
+    reqsData.name = "admin"
+    reqsData.pswd = "Admin123"
+    # print("Enter router's IP address")
+    # print("IP: ", end="")
+    # ipAddr = input()
+    reqsData.ipAddr = "192.168.1.4"
+    reqsData.baseURL = "http://"+reqsData.ipAddr+"/api/login"
+    LoginToken(reqsData)
+    # data = GetConfigData()
+    CheckForMobile(reqsData)
+
+
+def SendReq(reqsData, endpoint):
+    head = {"Content-Type": "application/json",
+            "Authorization": "Bearer " + reqsData.token}
+    response = requests.post(reqsData.baseURL+endpoint, headers=head).json()
+    if (response["success"] == False):
+        print(Colors.Red("Request failed"))
+        return -1
+    else:
+        print(Colors.Green("Data retrieved"))
+        return response
+
+
+def LoginToken(reqsData):
+    head = {"Content-Type": "application/json"}
+    creds = {"username": reqsData.name, "password": reqsData.pswd}
+    response = requests.post(reqsData.baseURL, json=creds, headers=head).json()
+    if (response["success"] == True):
+        reqsData.token = response["jwtToken"]
+    else:
+        sys.exit("Login was unsuccessful")
 
 
 def GetConfigData():
@@ -22,30 +55,35 @@ def GetConfigData():
     return data
 
 
-def LoginToDevice():
-    # print("Enter router's admin page credentials")
-    # print("Username: ", end="")
-    # name = input()
-    # pswd = getpass()
-    name = "admin"
-    pswd = "Admin123"
-    # print("Enter router's IP address")
-    # print("IP: ", end="")
-    # ipAddr = input()
-    ipAddr = "192.168.1.1"
+def CheckForMobile(reqsData):
+    SendReq(reqsData, "/system/device/info")
+    # print(reqs["data"])
+    # if (reqs["data"]["board"]["hwinfo"]["mobile"] == True):
+    #    Colors.Green+"Device has mobile capabilities"
+    # else:
+    #    Colors.Red+"Device does not have mobile capabilities"
 
-    url = "http://"+ipAddr+"/api/login"
-    creds = {"username": name, "password": pswd}
-    response = requests.post(url, json=creds).json()
-    return response["jwtToken"]
+    # def ReadEvents(file):
+    #    data = json.dumps(file)
 
 
-def CheckForMobile(ip, data):
-    # device's info
-    model = data["info"]["product"]
+class RequestData:
+    def __init__(self):
+        self.ipAddr = "",
+        self.token = "",
+        self.baseURL = "",
+        self.name = "",
+        self.pswd = ""
 
-# def ReadEvents(file):
-#    data = json.dumps(file)
+# Utilities
+
+
+class Colors():
+    def Green(text):
+        return "\033[42m"+text+"\033[0m"
+
+    def Red(text):
+        return "\033[41m"+text+"\033[0m"
 
 
 Main()
