49844fe5689920057d4cb592a7cbcc75fc6005f4
TautvydasVck <tautvydas.vck@protonmail.com>
2023-04-17 10:51:27 +0300

    update

--- modules/MessageDecode.py
+++ modules/MessageDecode.py
@@ -7,6 +7,5 @@ from modules.Requests import SendCommand
 
 def Decode():
     if(eventResults.messageOut.__contains__("%ie")):
-        res = SendCommand("gsmctl -i", dataSender)
-        eventResults.messageOut.replace("%ie", res[0][:-1])
-               
+        res = SendCommand("gsmctl -i", dataSender)        
+        eventResults.messageOut = eventResults.messageOut.replace("%ie", res[0][:-1])
