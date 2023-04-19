7d2c28d948c0d519a1496508862f726401bf7760
TautvydasVck <tautvydas.vck@protonmail.com>
2023-04-19 13:14:23 +0300

    update

--- modules/DataFile.py
+++ modules/DataFile.py
@@ -4,12 +4,20 @@ from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 from classes.Utilities import Text
+from modules.Variables import fileData
 
-def ReadDataFile(filePath):
-    try:
-        with open(filePath) as f:
-            data = json.load(f)
-        return data
+def ReadDataFile():
+    try:        
+        return LoadFile()
     except FileNotFoundError:
         print(Text.Red("JSON configuration file was not found"))
         sys.exit()
+
+def LoadFile():
+    try:
+        with open(fileData.dataFileName) as f:
+                data = json.load(f)
+        return data
+    except json.decoder.JSONDecodeError:
+         print(Text.Red("JSON file is misformed\nCheck configuration file"))
+         sys.exit()
--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from modules.PrimaryChecks import CheckForMobile, CheckForModel
 from modules.DataFile import ReadDataFile
 from modules.APIToken import GetToken
 from modules.Receiver import GetPhoneNumbers
-from modules.Variables import ReadArgs, fileData
+from modules.Variables import ReadArgs
 from modules.TestEventsSMS import TestEvents
 
 print(end="\n")
 ReadArgs()
 GetToken()
-data = ReadDataFile(fileData.dataFileName)
+data = ReadDataFile()
 GetPhoneNumbers(data)
 CheckForModel(data)
 CheckForMobile()
