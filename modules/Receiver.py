92eb90fb98e9455441fb20202313e04cdaf70269
TautvydasVck <tautvydas.vck@protonmail.com>
2023-04-25 08:15:52 +0300

    update

--- modules/Receiver.py
+++ modules/Receiver.py
@@ -13,37 +13,20 @@ def CheckReceive():
         print(Text.Yellow("Device did not receive the message\nAfter 20 seconds program will try to read the SMS again"))
         time.sleep(20)
         res = SendCommand("gsmctl -S -l all", dataReceiver)    
-    if (len(res) == 15):        
-        CheckContent(res, 1)
-    elif(len(res) > 15):
-        #need to fix this shit        
-        messageIndex = FindMessage(res)
-        CheckContent(res, messageIndex)
+    if (len(res) >= 15):        
+        CheckContent(res)    
     else:
         print(Text.Red("Device did not receive the message\nFailed"))        
-#need to fix this shit
-def CheckContent(message, messageIndex):
-    eventResults.received = message[2*messageIndex].split(":\t\t")[1][:-1]
-    eventResults.messageIn = message[13*messageIndex].split(":\t\t")[1][:-1]        
+
+def CheckContent(message):
+    eventResults.received = message[2].split(":\t\t")[1][:-1]
+    eventResults.messageIn = message[13].split(":\t\t")[1][:-1]        
     if (eventResults.received == deviceInfo.sims[deviceInfo.activeSim]
             and eventResults.messageIn == eventResults.messageOut):
         eventResults.passed = True
         print(Text.Green("Passed"))
     else:
         print(Text.Red("Failed"))
-#need to fix this shit
-#naudoti gsmctl -S -r <index> PAKEISTI metoda
-def LookThroughMessages(res):
-    indexes = GetMessagesIndexes(res)
-    cnt = 1
-    messageIndex = 0
-    while cnt <= len(indexes):
-        received = str(res[2*(cnt)].split(":\t\t")[1])
-        if(received.startswith(deviceInfo.sims[deviceInfo.activeSim])):
-            messageIndex = cnt-1
-            cnt = len(indexes)
-        cnt+=1
-    return messageIndex
 
 def GetPhoneNumbers(file):
     try:
