9a201ee07b7f5588b8cfeb824c7b08429fc0b378
TautvydasVck <tautvydas.vck@protonmail.com>
2023-04-26 13:41:12 +0300

    update

--- event-report-test.py
+++ event-report-test.py
@@ -10,36 +10,36 @@ from modules.ResultFile import CreateCSV, UploadCSV
 from modules.TestEventsSMS import TestEvents
 from modules.Variables import ReadArgs, deviceInfo, testResults
 
-if __name__ == "__main__":
-    print(end="\n")
-    testResults.startTime = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
-    ReadArgs()
-    # If one of these primary checks fails, program must be stopped
-    try:
-        GetToken()
-        data = ReadDataFile()
-        GetPhoneNumbers(data)
-        GetSysInfo()
-        CheckForModel(data)
-        CheckForMobile()
-        CheckReceiverConn()
-        CheckSenderGsm()
-        CheckTotalEvents(data)
-        print(Text.Underline("|   Primary checks passed   |"))
-        deviceInfo.sysInfo = ""
-    except Exception as err:
-        print(Text.Underline("|   Primary checks failed   |"))
-        print(Text.Red(str(err)))
-        sys.exit()
-    CreateCSV(data)
-    print("Started at: {0}".format(testResults.startTime))
-    print("Total tests: {0}\n".format(testResults.total))
-    TestEvents(data)
-    print("Total events tested: {0}".format(testResults.total))
-    print(Text.Green("Passed: {0}".format(testResults.passedCnt)), end=" ")
-    print(Text.Red("Failed: {0}".format(testResults.failedCnt)))
-    try:
-        UploadCSV()
-    except Exception:
-        print(Text.Yellow(str(err)))
-    print("|---------------FINISHED---------------|")
+
+print(end="\n")
+testResults.startTime = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+ReadArgs()
+# If one of these primary checks fails, program must be stopped
+try:
+    GetToken()
+    data = ReadDataFile()
+    GetPhoneNumbers(data)
+    GetSysInfo()
+    CheckForModel(data)
+    CheckForMobile()
+    CheckReceiverConn()
+    CheckSenderGsm()
+    CheckTotalEvents(data)
+    print(Text.Underline("|   Primary checks passed   |"))
+    deviceInfo.sysInfo = ""
+except Exception as err:
+    print(Text.Underline("|   Primary checks failed   |"))
+    print(Text.Red(str(err)))
+    sys.exit()
+CreateCSV(data)
+print("Started at: {0}".format(testResults.startTime))
+print("Total tests: {0}\n".format(testResults.total))
+TestEvents(data)
+print("Total events tested: {0}".format(testResults.total))
+print(Text.Green("Passed: {0}".format(testResults.passedCnt)), end=" ")
+print(Text.Red("Failed: {0}".format(testResults.failedCnt)))
+try:
+    UploadCSV()
+except Exception:
+    print(Text.Yellow(str(err)))
+print("|---------------FINISHED---------------|")
